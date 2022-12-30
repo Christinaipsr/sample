5c329046c95ed9d79abc4fb8aba6259be8e7d8f7
Christinaipsr <89643500+Christinaipsr@users.noreply.github.com>
2022-12-31 05:08:09 +0530

    Update Greeter.java

--- Greeter.java
+++ Greeter.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Greeter{	
 	public static void main(String[] args){
 		
-		System.out.println("What's your name? (Please type your name and press enter.");
+		System.out.println("What's your good name? (Please type your name and press enter.");
 		
 		Scanner scanner = new Scanner(System.in);
 		String name = scanner.nextLine();
